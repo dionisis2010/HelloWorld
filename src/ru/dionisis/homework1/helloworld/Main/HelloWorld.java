d934eaa3d7196457f0b6fec696c6ca63a7afed26
dionisis2010 <2010dionisis2010@gmail.com>
2019-10-29 14:52:36 +0300

    1232

--- src/ru/dionisis/homework1/helloworld/Main/HelloWorld.java
+++ src/ru/dionisis/homework1/helloworld/Main/HelloWorld.java
@@ -4,6 +4,15 @@ package ru.dionisis.homework1.helloworld.Main;
  * this is a minimal java program
  */
 public class HelloWorld {
+    String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
