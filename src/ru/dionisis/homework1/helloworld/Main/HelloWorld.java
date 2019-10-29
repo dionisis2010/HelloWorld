2c9fe30e4c7029a9fbf66bc020ca24e75c8bb8e9
dionisis2010 <2010dionisis2010@gmail.com>
2019-10-29 09:54:39 +0300

    поместил класс в пакет добавил жавадок

--- src/ru/dionisis/homework1/helloworld/Main/HelloWorld.java
+++ src/ru/dionisis/homework1/helloworld/Main/HelloWorld.java
@@ -1,7 +1,11 @@
+package ru.dionisis.homework1.helloworld.Main;
+
+/**
+ * this is a minimal java program
+ */
 public class HelloWorld {
+
     public static void main(String[] args) {
         System.out.println("Hello, world!");
-        int a = 10;
-        System.out.println((char)101);
     }
 }
