010c8c4d63b2c45ceff6e5995009631ed8153d26
dionisis2010 <2010dionisis2010@gmail.com>
2019-10-29 10:05:06 +0300

    comment 1

--- src/ru/dionisis/homework1/helloworld/Main/HelloWorld.java
+++ src/ru/dionisis/homework1/helloworld/Main/HelloWorld.java
@@ -4,6 +4,7 @@ package ru.dionisis.homework1.helloworld.Main;
  * this is a minimal java program
  */
 public class HelloWorld {
+    // comment1
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
