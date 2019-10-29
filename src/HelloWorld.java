9c77722b56573b6afaaf20ef169caa331bdbdcad
dionisis2010 <2010dionisis2010@gmail.com>
2019-10-29 09:48:25 +0300

    Hello, World! Первое дамашнее задание

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        int a = 10;
+        System.out.println((char)101);
     }
 }
