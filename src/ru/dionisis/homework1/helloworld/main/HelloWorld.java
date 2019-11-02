a58afb254b98484d9ad2c4b664b87c46e1f52c8d
dionisis2010 <2010dionisis2010@gmail.com>
2019-11-02 22:12:18 +0300

    добавил гитигнор удалил комменты переименовал package

--- src/ru/dionisis/homework1/helloworld/main/HelloWorld.java
+++ src/ru/dionisis/homework1/helloworld/main/HelloWorld.java
@@ -5,15 +5,7 @@ package ru.dionisis.homework1.helloworld.main;
  */
 public class HelloWorld {
     public static void main(String[] args) {
-//        A a= new A();
-//        B b = new B();
-//        System.out.println(a instanceof A);
+
         System.out.println("Hello, world!");
     }
 }
-//class A {
-//
-//}
-//class B extends A{
-//
-//}
